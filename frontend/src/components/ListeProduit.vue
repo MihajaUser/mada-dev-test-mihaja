e1ec0578f96bba239baf6b0ce974cc23e7b8852e
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2023-02-23 14:39:53 +0300

    CRUD success full

--- frontend/src/components/ListeProduit.vue
+++ frontend/src/components/ListeProduit.vue
@@ -1,38 +1,36 @@
 <template>
     <div class="app">
-        {{ product }}
-        <h1>CRUD Interface</h1>
-        <form>
-            <h2>Créer une nouvelle entrée</h2>
+        <h1>CRUD Produit test Mada dev</h1>
+        <h2>Créer une nouveau produit</h2>
+        <form @submit.prevent="formCreation">
             <label for="name">Name :</label>
-            <input type="text" id="price" name="price" required>
-
+            <input type="text" id="price" name="price" v-model="name" required>
             <label for="price">Price :</label>
-            <input type="text" id="price" name="price" required>
-
+            <input type="text" id="price" name="price" v-model="price" required>
             <label for="description">Description :</label>
-            <textarea id="description" name="description"></textarea>
+            <input id="description" name="description" v-model="description">
             <button type="submit">Créer</button>
         </form>
-
         <h2>Liste des produits</h2>
-        <table>
+        <table class="styled-table">
             <thead>
                 <tr>
-                    <th>Nom</th>
-                    <th>Prix</th>
+                    <th>Name</th>
+                    <th>Price</th>
                     <th>Description</th>
                     <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Nom de l'entrée</td>
-                    <td>Description de l'entrée</td>
+                <tr v-for="item in products " :key="item.id" class="active-row">
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.price }} Ar</td>
+                    <td>{{ item.description }}</td>
                     <td>
                         <button>Editer</button>
-                        <button>Supprimer</button>
+                        <form @submit.prevent="formDelete(item.id)">
+                            <button>Supprimer</button>
+                        </form>
                     </td>
                 </tr>
             </tbody>
@@ -42,29 +40,45 @@
 <script lang="ts">
 
 import { defineComponent, ref } from 'vue';
-import { getProduct } from '@/service/productService.api';
+import { getProduct, createProduct, deleteProduct } from '@/service/productService.api';
 export default defineComponent({
     data() {
         return {
-            product: null,
+            products: null,
         }
     },
     async created() {
         try {
-            const result = await await getProduct();
-            this.product = result?.data
+            const result = await getProduct();
+            this.products = result?.data
+            console.log(this.products)
         } catch (error) {
             console.log(error);
         }
     },
     methods: {
-        myMessage() {
-            console.log("my message mihaja")
-            // return "my message";
+        async formCreation() {
+            try {
+                await createProduct(this.name, this.price, this.description);
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        async formDelete(id: any) {
+            try {
+                await deleteProduct(id);
+            } catch (error) {
+                console.log(error);
+            }
         }
+        // ,
+        // async formUpdate() {
+        //     try {
+        //         await updateProduct(this.id, this.name, this.price, this.description)
+        //     } catch (error) {
+        //         console.log(error);
+        //     }
+        // }
     }
-
 });
-
-
 </script>
